8a5bc8d794200fa961d778038d623babfa4f421b
delbrugge <delbrugge@gmail.com>
2021-01-26 17:09:38 -0500

    changes made to card

---
+++ build/precache-manifest.d33d6f92aa464755bd1cd73de94ff9b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb76d78561a482c0a2f1368198dbb36d",
+    "url": "/covidTracker/index.html"
+  },
+  {
+    "revision": "417504eb17da3c54f4fc",
+    "url": "/covidTracker/static/css/main.af04aae6.chunk.css"
+  },
+  {
+    "revision": "9b47a8d89a0a6ab15e8b",
+    "url": "/covidTracker/static/js/2.108fb10c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidTracker/static/js/2.108fb10c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "417504eb17da3c54f4fc",
+    "url": "/covidTracker/static/js/main.ba09ad8d.chunk.js"
+  },
+  {
+    "revision": "545f4f0d00ab31967e03",
+    "url": "/covidTracker/static/js/runtime-main.8e5962f2.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidTracker/static/media/image.d7265326.png"
+  }
+]);
