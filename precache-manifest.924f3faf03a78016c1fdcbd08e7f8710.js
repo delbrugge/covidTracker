434d24877fa6ced42fe01c2728b585b72ea07898
delbrugge <delbrugge@gmail.com>
2021-01-26 16:51:59 -0500

    Updates

---
+++ precache-manifest.924f3faf03a78016c1fdcbd08e7f8710.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8cbecc286a07479deba94c52e3e95d",
+    "url": "/delbrugge/covidTracker/index.html"
+  },
+  {
+    "revision": "417504eb17da3c54f4fc",
+    "url": "/delbrugge/covidTracker/static/css/main.af04aae6.chunk.css"
+  },
+  {
+    "revision": "ab3c97c150842c461e87",
+    "url": "/delbrugge/covidTracker/static/js/2.9f922c60.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/delbrugge/covidTracker/static/js/2.9f922c60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "417504eb17da3c54f4fc",
+    "url": "/delbrugge/covidTracker/static/js/main.ba09ad8d.chunk.js"
+  },
+  {
+    "revision": "c8e1cdc730348c753319",
+    "url": "/delbrugge/covidTracker/static/js/runtime-main.d08ca487.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/delbrugge/covidTracker/static/media/image.d7265326.png"
+  }
+]);
